7b6be5d01988d780f312eb054df4f1b3a7e1d725
narendraprasathnp7@gmail.com <narendraprasathnp7@gmail.com>
2022-03-22 14:12:16 +0530

    code change

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.get("/api/config/paypal", (req, res) => {
 
 
 if(process.env.NODE_ENV === 'production'){
-app.use(express.static(path.join(__dirname, "/frontend/build")))
+app.use(express.static(path.join(__dirname, "/client/build")))
 app.get('*', (req,res) =>{
-  res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+  res.sendFile(path.resolve(__dirname+"client/build/index.html"))
 });
 }else{
   app.get("/", (req, res) => {
